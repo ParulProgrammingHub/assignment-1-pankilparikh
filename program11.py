3587a541e6a1308beab62beb7d5f45e3798f89d8
pankilparikh <150303105104@paruluniversity.ac.in>
2017-02-09 22:21:11 +0530

    Create program11.py

---
+++ program11.py
@@ -0,0 +1,7 @@
+principle=int(input(“enter the principle amount:”))
+rate=int(input(“enter the rate:”))
+time=int(input(“enter the time:”))
+def simple_interest(principle,time,rate):
+P=principle*(1+rate*time)
+Print(“total simple interest:”,P)
+Simple_interest(principle,time,rate)
